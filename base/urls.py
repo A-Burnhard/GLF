566083bff6c47ad863185c0a187e4276bcefbb22
Appiedu <benadkojo@gmail.com>
2022-09-06 10:54:16 +0000

    all uploaded through vs code

--- base/urls.py
+++ base/urls.py
@@ -1,2 +1,13 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import VolunteerView, HomeView, SuccessPage200
+from django.views.generic import TemplateView
+urlpatterns = [
+    path('vol/', VolunteerView.as_view(), name = 'volform'),
+    path('home/', HomeView.as_view(), name= 'home' ), 
+    path('success/', SuccessPage200.as_view() , name ='success'),
+    path('overview/',TemplateView.as_view(template_name = 'pages/index.html')),
+    path('test/',TemplateView.as_view(template_name = 'pages/login.html')),
+
+
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.views import View
+from .models import Tour, Volunteer
+from .forms import VolunteerForm
+
+class VolunteerView(FormView):
+
+    template_name = 'volunteer.html'
+    form_class = VolunteerForm
+    success_url = reverse_lazy('Success')
+
+class HomeView(ListView):
+
+    template_name = 'home.html'
+    success_url = reverse_lazy('home')
+
+
+class SuccessPage200(ListView):
+    model = Volunteer
+    template_name = 'Success.html'
+    context_object_name = 'success'
+    success_url = reverse_lazy('success')
+   
