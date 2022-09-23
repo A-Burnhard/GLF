399c48a62ab64355fe2285200a706fa027fafd08
Appiedu <benadkojo@gmail.com>
2022-09-23 11:32:55 +0000

    api added thinking of mixins

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+from base.models import Volunteer,Tour, Donor
+from base.serializers import VolunteerSerializer, TourSerializer, DonorSerializer
+
+@api_view(['POST'])
+def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    serializer = VolunteerSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        # instance = form.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
--- base/serializers.py
+++ base/serializers.py
@@ -4,17 +4,17 @@ from rest_framework import serializers
 
 from .models import Volunteer, Donor, Tour
 
-class VolunteerSerializer(serializers.Modelserializer):
+class VolunteerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Volunteer
         fields = ['__all__']
 
-class DonorSerializer(serializers.Modelserializer):
+class DonorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Donor
         fields = ['__all__']
 
-class TourSerializer(serializers.Modelserializer):
+class TourSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tour
         fields = ['__all__']
