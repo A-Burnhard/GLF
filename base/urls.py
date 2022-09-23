c7a55a2cabb5e5f3f1f891acc43fee17f28902ed
Appiedu <benadkojo@gmail.com>
2022-09-23 20:09:56 +0000

    changes

--- base/urls.py
+++ base/urls.py
@@ -10,14 +10,18 @@ from base.views import *
 urlpatterns = [
     #CreateAPI routers
     path('', HomePage.as_view(), name ="home"),
-    path('volunteer/', VolunteerCreateAPIView.as_view(), name="vol_url"),
-    path('donor/', DonorCreateAPIView.as_view(), name="don_url"),
-    path('tour/', TourCreateAPIView.as_view(), name="tour_url"),
+    path('volunteer/', VolunteerCreateAPIView.as_view(), name="vol_post"),
+    path('donor/', DonorCreateAPIView.as_view(), name="don_post"),
+    path('tour/', TourCreateAPIView.as_view(), name="tour_post"),
 
     #DetailAPI routers
-    path('volunteerdetail/', VolunteerDetailAPIView.as_view(), name="vol_url"),
-    path('donordetail/', DonorDetailAPIView.as_view(), name="don_url"),
-    path('tourdetail/', TourDetailAPIView.as_view(), name="tour_url"),
+    path('volunteerdetail/', VolunteerDetailAPIView.as_view(), name="vol_detail"),
+    path('donordetail/', DonorDetailAPIView.as_view(), name="don_detail"),
+    path('tourdetail/', TourDetailAPIView.as_view(), name="tour_detail"),
 
+    #ListAPI Routers
+    path('volunteerlist/', VolunteerListAPIView.as_view(), name="vol_list"),
+    path('donorlist/', DonorListAPIView.as_view(), name="don_list"),
+    path('tourlist/', TourListAPIView.as_view(), name="tour_list"),
 
 ]
