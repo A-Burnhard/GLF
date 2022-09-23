ddb6336e4e392b36fa754378b83508bba5a1a2a5
Appiedu <benadkojo@gmail.com>
2022-09-23 19:59:58 +0000

    configured routers

--- base/urls.py
+++ base/urls.py
@@ -8,15 +8,16 @@ from base.views import *
 
 
 urlpatterns = [
-
+    #CreateAPI routers
     path('', HomePage.as_view(), name ="home"),
     path('volunteer/', VolunteerCreateAPIView.as_view(), name="vol_url"),
     path('donor/', DonorCreateAPIView.as_view(), name="don_url"),
     path('tour/', TourCreateAPIView.as_view(), name="tour_url"),
 
- #   path('volunteer/', VolunteerDetailAPIView.as_view(), name="vol_url"),
-  #  path('donor/', DonorDetailAPIView.as_view(), name="don_url"),
- #   path('tour/', TourDetailAPIView.as_view(), name="tour_url"),
+    #DetailAPI routers
+    path('volunteerdetail/', VolunteerDetailAPIView.as_view(), name="vol_url"),
+    path('donordetail/', DonorDetailAPIView.as_view(), name="don_url"),
+    path('tourdetail/', TourDetailAPIView.as_view(), name="tour_url"),
 
 
 ]
