eb6cb59f2b41cf577a8650c74dfdf66fdcd11a28
Appiedu <benadkojo@gmail.com>
2022-09-24 15:57:32 +0000

    hmmn

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,38 @@
+from msilib.schema import AdminExecuteSequence
+from multiprocessing import Value, managers
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Superadmin(AbstractUser):
+    username = models.CharField(max_length=150, null=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True, blank=True)
+    bio = models.TextField(null=True)
+    is_staff = models.BooleanField()
+
+    avatar = models.ImageField(null=True)
+
+class admin(AbstractUser):
+    username = models.CharField(max_length=150, null=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True, blank=True)
+    bio = models.TextField(null=True)
+    is_staff = models.BooleanField()
+
+    avatar = models.ImageField(null=True)
+
+class Staff(AbstractUser):
+    username = models.CharField(max_length=150, null=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True, blank=True)
+    bio = models.TextField(null=True)
+    is_staff = models.BooleanField()
+
+    avatar = models.ImageField(null=True)
+
+
+
