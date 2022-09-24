8d9f341d35e6f6db6260527374f494e500dafd8f
Appiedu <benadkojo@gmail.com>
2022-09-24 02:37:58 +0000

    removed

--- manager/urls.py
+++ manager/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import *
 
 urlpatterns = [
 
-    path('', HomePageView.as_view(), name='index.html')
+    path('', DashBoard.as_view(), name='index.html')
 
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,10 +1,11 @@
 from django.views.generic.base import TemplateView
-from django.http import HttpResponse
-from django.urls import reverse_lazy
-from django.shortcuts import render
-from django.views import View
 
-class HomePageView(TemplateView):
 
-  template_name = "index.html"
-  SUCCESS_URL = reverse_lazy('index.html')
+from rest_framework import generics, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.shortcuts import get_list_or_404
+
+class DashBoard(TemplateView):
+    template_name = "manager/TEMPLATE.html"
