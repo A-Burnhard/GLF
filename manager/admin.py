bad895465f31cdaf95a0f7208315c34dab989b8f
Appiedu <benadkojo@gmail.com>
2022-09-24 16:18:35 +0000

    j

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Superadmin, Superstaff,Staff
 # Register your models here.
+
+admin.site.register(Superadmin)
+admin.site.register(Superstaff)
+admin.site.register(Staff)
