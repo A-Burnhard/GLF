d1b15511a292af2c5c66737629486b2c7920bea7
Appiedu <benadkojo@gmail.com>
2022-11-29 13:15:42 +0000

    updated

--- GLF/settings.py
+++ GLF/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0@9n6uex-46l+w#(87-yd##p@zn$n4+0!#n2ln)(y^ha1ll^%x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 if not DEBUG:
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['glf-test.up.railway.app']
 
 
 
