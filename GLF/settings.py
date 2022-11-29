25e6b9599e348118cf6a6fb1d951c8d1668670c3
Appiedu <benadkojo@gmail.com>
2022-11-29 10:25:10 +0000

    updated

--- GLF/settings.py
+++ GLF/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',    
+
     'base.apps.BaseConfig',
     'manager.apps.ManagerConfig',
     'api.apps.ApiConfig',
